83e7911c47a309722497cfcaa08964b56247b702
TShahedi <t.shahedi@uu.nl>
2024-07-25 14:49:17 +0200

    Initial commit

---
+++ R/my_function.R
@@ -0,0 +1,11 @@
+#' Title of your function
+#'
+#' Description of your function.
+#'
+#' @param x Description of parameter x.
+#' @return Description of the return value.
+#' @export
+my_function <- function(x) {
+  # Your function code
+  x + 1
+}
---
+++ tests/testthat/test-my_function.R
@@ -0,0 +1,3 @@
+test_that("my_function works correctly", {
+  expect_equal(my_function(1), 2)
+})
